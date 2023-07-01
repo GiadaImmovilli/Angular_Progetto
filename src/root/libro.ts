57464bcafe48eb706d8984ed1b0828f6715aa887
GiadaImmovilli <116741439+GiadaImmovilli@users.noreply.github.com>
2023-07-01 11:50:06 +0200

    Inizializzazione

---
+++ src/root/libro.ts
@@ -0,0 +1,18 @@
+export class Libro {
+  titolo: string;
+  autore: string;
+  posizione: string;
+  nominativo: string;
+
+  constructor(
+    titolo: string,
+    autore: string,
+    posizione: string,
+    nominativo: string
+  ) {
+    this.titolo = titolo;
+    this.autore = autore;
+    this.posizione = posizione;
+    this.nominativo = nominativo;
+  }
+}
