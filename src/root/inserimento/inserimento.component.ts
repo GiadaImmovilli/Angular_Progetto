00c759dc58578e9e0460ee72070fd726265eb613
GiadaImmovilli <116741439+GiadaImmovilli@users.noreply.github.com>
2023-07-02 17:22:48 +0200

    2 files modified

--- src/root/inserimento/inserimento.component.ts
+++ src/root/inserimento/inserimento.component.ts
@@ -16,5 +16,30 @@ export class InserimentoComponent implements OnInit {
 
   @Input() inserisci: boolean;
 
-  public inserimentoLibro() {}
+  inserimentoLibro() {
+    var inputTitolo: HTMLInputElement = document.getElementById(
+      'titoloInserito'
+    ) as HTMLInputElement;
+    var inputAutore: HTMLInputElement = document.getElementById(
+      'autoreInserito'
+    ) as HTMLInputElement;
+    var inputPosizione: HTMLInputElement = document.getElementById(
+      'posizioneInserita'
+    ) as HTMLInputElement;
+
+    var nuovoTitolo = inputTitolo.value;
+    var nuovoAutore = inputAutore.value;
+    var nuovaPosizione = inputPosizione.value;
+
+    // document.getElementById('output2').innerHTML =
+    //   inputTitolo.value + ' ' + inputAutore.value + ' ' + inputPosizione.value;
+
+    inputTitolo.value = '';
+    inputAutore.value = '';
+    inputPosizione.value = '';
+  }
+
+  tornaIndietro() {
+    // this.inserisci = false;
+  }
 }
