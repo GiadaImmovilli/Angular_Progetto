98c2774059410b29f357c091b7cc0a29ec0d1bd5
GiadaImmovilli <116741439+GiadaImmovilli@users.noreply.github.com>
2023-07-04 16:26:07 +0200

    3 files modified

--- src/root/ricerca/ricerca.component.ts
+++ src/root/ricerca/ricerca.component.ts
@@ -21,23 +21,56 @@ export class RicercaComponent implements OnInit {
 
   ngOnInit() {}
 
-  ricercaLibro() {
-    this.nascondiRicerca();
+  ricercaSingoloLibro() {
+    // var nuovoArchivio = new Archivio(this.archivioAppoggio);
+
+    this.archivioAppoggio.getDB().subscribe({
+      next: (res: AjaxResponse<any>) => {
+        this.archivio = JSON.parse(res.response);
+      },
+      error: (err: AjaxError) => console.error(err.response),
+    });
+
+    var inputStringa: HTMLInputElement = document.getElementById(
+      'stringaInput'
+    ) as HTMLInputElement;
+    // conterrà i libri che corrispondono alla stringa inserita
+    var libriTrovati: Array<Libro> = [];
+    // conterrà il numero di libri trovati corrispondenti alla stringa inserita
+    var occorrenze: HTMLInputElement = document.getElementById(
+      'risultatoOccorrenze'
+    ) as HTMLInputElement;
+    // conterrà il testo relativo al libro trovato (corrispondenze=1) corrispondente alla stringa inserita
+    var risultato: HTMLInputElement = document.getElementById(
+      'risultatoRicerca'
+    ) as HTMLInputElement;
+
+    var stringa = inputStringa.value;
+
+    // if (stringa === '') {
+    //   occorrenze.value = 'Nessun libro trovato';
+    // } else {
+    //   this.archivio.forEach((singoloLibro) =>
+    //     this.archivio.ricercaLibri(stringa, singoloLibro, libriTrovati)
+    //   );
+
+    //   occorrenze.value = '';
+
+    //   if (libriTrovati.length == 1) {
+    //     libriTrovati.forEach(
+    //       (singoloLibro) =>
+    //         (risultato.value =
+    //           ' "' + singoloLibro['titolo'] + '" ' + singoloLibro['autore'])
+    //     );
+    //   } else {
+    //     occorrenze.value = 'Libri trovati: ' + libriTrovati.length;
+    //   }
+    // }
+    this.nascondiRicerca(); // la pagina deve tornare alla home
   }
 
   nascondiRicerca() {
     this.ricerca = false;
     this.nascondi.emit(this.ricerca);
   }
-  // var nuovoArchivio = new Archivio(this.archivioAppoggio);
-
-  //   this.archivioAppoggio.getDB().subscribe({
-  //     next: (res: AjaxResponse<any>) => {
-  //       this.archivio = res.response;
-  //       // document.getElementById('output').innerHTML = String(this.archivio);
-  //       this.archivio = JSON.parse(res.response);
-  //       nuovoArchivio.inserimentoLibro(nuovoLibro, this.archivio);
-  //     },
-  //     error: (err: AjaxError) => console.error(err.response),
-  //   });
 }
